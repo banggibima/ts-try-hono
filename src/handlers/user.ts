6409943654bb390cbf4eb8a9424f53f4335d8800
Banggi Bima <banggibima@gmail.com>
2024-08-10 22:43:32 +0700

    feat: adjust handler

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -6,11 +6,17 @@ import {
   insertUserService,
   updateUserService,
 } from "../services/user";
+import { create, update } from "../validators/user";
+import { User } from "../models/user";
 
 export const getUsers = async (c: Context) => {
-  const users = await getUsersService();
+  try {
+    const users = await getUsersService();
 
-  return c.json(users);
+    return c.json(users, 200);
+  } catch (error) {
+    return c.json({ error: error }, 500);
+  }
 };
 
 export const getUserById = async (c: Context) => {
@@ -18,30 +24,66 @@ export const getUserById = async (c: Context) => {
 
   const user = await getUserByIdService(id);
 
-  return c.json(user);
+  if (user === null) {
+    return c.json(user, 404);
+  }
+
+  return c.json(user, 200);
 };
 
 export const insertUser = async (c: Context) => {
   const payload = await c.req.json();
 
-  const user = await insertUserService(payload);
+  const validation = create.safeParse(payload);
 
-  return c.json(user);
+  if (validation.success === false) {
+    return c.json({ error: validation.error.issues }, 400);
+  }
+
+  try {
+    const user = await insertUserService(validation.data as User);
+
+    return c.json(user, 201);
+  } catch (error) {
+    return c.json({ error: error }, 500);
+  }
 };
 
 export const updateUser = async (c: Context) => {
   const id = c.req.param("id");
   const payload = await c.req.json();
 
-  const user = await updateUserService(id, payload);
+  const validation = update.safeParse(payload);
 
-  return c.json(user);
+  if (validation.success === false) {
+    return c.json({ error: validation.error.issues }, 400);
+  }
+
+  try {
+    const user = await updateUserService(id, validation.data as User);
+
+    if (user === null) {
+      return c.json(user, 404);
+    }
+
+    return c.json(user, 200);
+  } catch (error) {
+    return c.json({ error: error }, 500);
+  }
 };
 
 export const deleteUser = async (c: Context) => {
   const id = c.req.param("id");
 
-  const user = await deleteUserService(id);
+  try {
+    const user = await deleteUserService(id);
 
-  return c.json({ id });
+    if (user === null) {
+      return c.json(user, 404);
+    }
+
+    return c.json(user, 204);
+  } catch (error) {
+    return c.json({ error: error }, 500);
+  }
 };
